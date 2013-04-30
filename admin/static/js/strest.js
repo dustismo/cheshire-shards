1b8caaed7056dd004d692629eba2fb4d1acd14cb
Dustin Norlander <dustinn@gmail.com>
2013-04-30 16:38:56 -0700

    some convenient methods in strest

--- admin/static/js/strest.js
+++ admin/static/js/strest.js
@@ -316,6 +316,34 @@ StrestResponse.prototype.toString = function() {
 	return this.originalText;
 };
 
+StrestResponse.prototype.getStatusCode = function() {
+	return this['status']['code']
+};
+
+
+StrestResponse.prototype.getStatusMessage = function() {
+	return this['status']['message']
+};
+
+/**
+ * Converts a form into a js object
+ */
+Strest.formToObject = function(obj) {
+    var o = {};
+    var a = $(obj).serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+}
+
 Strest.isString= function(obj) {
 	if (!obj) {
 		return false;

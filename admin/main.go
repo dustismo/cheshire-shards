3e2dd9b3f20e46ff6b52be246c24fda9c140094c
Dustin Norlander <dustinn@gmail.com>
2013-05-01 16:42:59 -0700

    update main

--- admin/main.go
+++ admin/main.go
@@ -7,8 +7,8 @@ import (
     "github.com/dustismo/cheshire-shards/admin/balancer"
     "github.com/trendrr/cheshire-golang/cheshire/impl/gocache"
     "flag"
-    "time"
-    "fmt"
+    // "time"
+    // "fmt"
 )
 
 
@@ -31,18 +31,30 @@ func main() {
     balancer.Servs.DataDir = *dataDir
     balancer.Servs.Load()
 
-    balancer.Servs.SetRouterTable(shards.NewRouterTable("Test"))
+    testrt := shards.NewRouterTable("Test")
+    balancer.Servs.SetRouterTable(testrt)
+
+    //try creating entry
+    entry := &shards.RouterEntry{
+        Address : "localhost",
+        JsonPort : 8009,
+        HttpPort : 8010,
+        Partitions : make([]int, 0),
+    }
+
+    log.Println("********************* ADD ENTRY")
+    testrt.AddEntries(entry)
 
     //
     log.Println("Starting")
-    go func() {
-        c := time.Tick(5 * time.Second)
-        for now := range c {
-            str := fmt.Sprintf("%v %s\n", now, "Something something")
-            balancer.Servs.Logger.Emit("test", str)
-            balancer.Servs.Logger.Println("TESTING LOG")
-        }    
-    }()
+    // go func() {
+    //     c := time.Tick(5 * time.Second)
+    //     for now := range c {
+    //         str := fmt.Sprintf("%v %s\n", now, "Something something")
+    //         balancer.Servs.Logger.Emit("test", str)
+    //         balancer.Servs.Logger.Println("TESTING LOG")
+    //     }    
+    // }()
     
 
     //starts listening on all configured interfaces
